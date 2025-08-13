ec499eaa66eedc8abc848477a7c201a87ba0efcc
Tomfmp2 <222234754+Tomfmp2@users.noreply.github.com>
2025-08-13 08:50:32 -0500

    - Se modificio la estructura logica - Se guardo en def diferentes cada tipo proceso para almacenar un dato al momento de registrar
    - Se seguiran haciendo cambios y mejorando el orden de la logica
    
    versio 1.3

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,5 +1,4 @@
 import os
-from email.utils import parseaddr
 
 cuentas_bancarias = {
     
@@ -24,111 +23,107 @@ def mostrar_menu_principal():
     print("6. Cancelar Cuenta")
     print("7. Salir")
     print("-----------------------------")
-
-def crear_cuenta(cuentas_bancarias):
-    #crear una nueva cuenta bancaria.
+def  registrar_nombre():
+    limpiar()
+    print('------------------------------')
+    print('     DATOS PERSONALES ')
+    print('------------------------------')
+            
     while True:
-        try:
-            while True:
-                # crearr nombre y contraseña de la cuenta
-                nombre = input("Ingrese el nombre del titular de la cuenta: ").strip().lower()
-                if not nombre:
-                    print("El nombre no puede estar vacío.")
-                    continue
-                break
-            # verificar si la cuenta ya existe
-            if nombre in cuentas_bancarias:
-                print("El nombre ya está registrado. Por favor, elija otro nombre.")
+            # crearr nombre y contraseña de la cuenta
+            nombre = input("Ingrese el nombre del titular de la cuenta: ").strip().lower()
+            if not nombre:
+                print("El nombre no puede estar vacío.")
                 continue
-            # Usuario ingresa la contraseña a crear 
-
-            while True:
-                contraseña = input("Ingrese una contraseña para la cuenta: ").strip()
-                if not contraseña:
-                    raise ValueError("La contraseña no puede estar vacía.")
-                    continue
-                else: 
-                    break
-            limpiar()
-            print('------------------------------')
-            print('     DATOS PERSONALES ')
-            print('------------------------------')
-            
-            while True:
-                edad = input('Ingrese su edad: ')
-                if not edad.strip():  # Si está vacío o solo espacios
-                    print('La edad no puede estar vacía.')
-                    continue
-                if not edad.isdigit():  # Si no son solo números
-                    print('La edad debe ser un número.')
-                    continue
-                else:
-                    break
-            while True:
-                limpiar()
-                print('-------------------------')
-                print('  1. Tefono Fijo.')
-                print('  2. Telefono Movil.')
-                print('-------------------------')
-                tipo_contacto = input('Ingrese el tipo de contacto que desea registras: ')
-                match tipo_contacto:
-                    case '1':
-                        numero_movil = int(input('Ingresa el numero de tu telefono movil: '))
-                        if not numero_movil.strip():  # Si está vacío o solo espacios
-                            print('El numero movil no puede estar vacío.')
-                            continue
-                        if not numero_movil.isdigit():  # Si no son solo números
-                            print('El numero movil solo debe tener números.')
-                            continue
-                    case '2':
-                        numero_fijo = int(input('Ingresa el numero de tu telefono Fijo: '))
-                        if not numero_movil.strip():  # Si está vacío o solo espacios
-                            print('El numero Fijo no puede estar vacío.')
-                            continue
-                        if not numero_movil.isdigit():  # Si no son solo números
-                            print('El numero Fijo solo debe tener números.')
-                            continue
-                    case '3':
-                        numero_movil = int(input('Ingresa el numero de tu telefono movil: '))
-                        if not numero_movil.strip():  # Si está vacío o solo espacios
-                            print('El numero movil no puede estar vacío.')
-                            continue
-                        if not numero_movil.isdigit():  # Si no son solo números
-                            print('El numero movil solo debe tener números.')
-                            continue
-                        numero_fijo = int(input('Ingresa el numero de tu telefono Fijo: '))
-                        if not numero_movil.strip():  # Si está vacío o solo espacios
-                            print('El numero Fijo no puede estar vacío.')
-                            continue
-                        if not numero_movil.isdigit():  # Si no son solo números
-                            print('El numero Fijo solo debe tener números.')
-                            continue
-                    case _:
-                        pass
-            while True:
-                email = input('Ingrese un correo electronico: ')
-                # Verificacion de correo electronico 
-                if "@" in parseaddr(email)[1]:
-                    print("Correo válido")
-                    break
-                else:
-                    print("Correo inválido") 
-                    continue
-            
-            # crear y adjuntar valores
-            """
-            datos = {'nombre','edad','email','cc','movil','fijo'}
-            valores = {nombre,}
-            print('La cuenta registrada con nombre: ',)
-            """
-            # Crear otra cuenta si el usuario lo desea
-            crear_otra_cuenta = input('¿Desea crear otra cuenta? (s/n): ').strip().lower()
-            if crear_otra_cuenta != 's':
-                break       
-        except ValueError as e:
-            print(f'Error: {e}, por favor solo ingresar letras.') 
+            else:
+                return nombre        
+def registrar_contraseña():
+    # Usuario ingresa la contraseña a crear             
+    while True:
+        contraseña = input("Ingrese una contraseña para la cuenta: ").strip()
+        if not contraseña:
+            print("La contraseña no puede estar vacía.")
             continue
-
+        else: 
+            return contraseña
+            
+def registro_numero_contacto():
+    limpiar()
+    print('-------------------------')
+    print('  1. Telefono Movil.  ')
+    print('  2. Tefono Fijo.     ')                                  
+    print('-------------------------')
+    tipo_contacto = input('Ingrese el tipo de contacto que desea registras: ')
+    while True:
+        match tipo_contacto:
+            case '1':
+                numero_contacto = input('Ingresa el numero de tu telefono movil: ')
+                if not numero_contacto.strip():  # Si está vacío o solo espacios
+                    print('El numero movil o puede estar vacío.')
+                    continue
+                if not numero_contacto.isdigit():  # Si no son solo números
+                    print('El numero movil solo debe tener números.')
+                    continue
+                else:
+                    return numero_contacto
+            case '2':
+                numero_contacto = input('Ingresa el numero de tu telefono Fijo: ')
+                if not numero_contacto.strip():  # Si está vacío o solo espacios
+                    print('El numero Fijo no puede estar vacío.')
+                    continue
+                if not numero_contacto.isdigit():  # Si no son solo números
+                    print('El numero Fijo solo debe tener números.')
+                    continue
+                else:
+                    return numero_contacto
+                
+                
+def registrar_edad():
+    while True:
+        edad = input('Ingrese su edad: ')
+        if not edad.strip():  # Si está vacío o solo espacios
+            print('La edad no puede estar vacía.')
+            continue
+        if not edad.isdigit():  # Si no son solo números
+            print('La edad debe ser un número.')
+            continue
+        else:
+            return edad
+            
+def registrar_documento():
+    while True:
+        cc = input('Ingrese su numero de cedula: ').strip()
+        if not cc.isdigit():  # Si no son solo números
+            print('El numero de cedula solo debe tener numeros. ')
+        else:
+            return cc
+            
+def registrar_ubicacion():
+    pass
+def registrar_email():
+    while True:
+        email = input('Ingrese un correo electronico: ')
+        # Verificacion de correo electronico 
+        if '@gmail.com'or '@hotmail.com' in email and len(email) > 11:
+            print("Correo válido")
+            return email
+        else:
+            print("Correo inválido") 
+            continue
+    
+def crear_cuenta():
+    nombre = registrar_nombre()
+    edad = registrar_edad()
+    documento = registrar_documento()
+    email = registrar_email()
+    numero_contacto = registro_numero_contacto()
+    #crear una nueva cuenta bancaria.
+    #adjuntar valores a los datos
+    datos = {'nombre','edad','email','cc','movil','fijo'}
+    valores = {nombre,edad,email,documento,numero_contacto}
+    cuenta_creada = dict(zip(datos,valores))
+    print(cuenta_creada)
+    return cuenta_creada
 
 
 # Ciclo principal del programa
@@ -138,7 +133,21 @@ while True:
         opcion = input("Seleccione una opción: ")
         match opcion:
             case '1':
-                crear_cuenta(cuentas_bancarias)
+                crear_otra_cuenta = 's'
+                while crear_otra_cuenta == 's':
+                        registrar_nombre()
+                        registrar_documento()
+                        registrar_edad()
+                        registrar_email()
+                        registro_numero_contacto()
+                        crear_cuenta()
+                        crear_mas_cuentas = input('¿Desea crear otra cuenta?: ').lower()
+                        if crear_mas_cuentas == 's':
+                            print('okey...')
+                        elif crear_mas_cuentas == 'n':
+                            break
+                        else:
+                            print('el dato ingresado no es correcto')
             case '2':
                 pass
             case '3':
